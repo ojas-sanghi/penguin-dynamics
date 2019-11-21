ae41723a4c467b78553e013d93ff410542b0d5f3
CalculusMaster <31602102+CalculusMaster@users.noreply.github.com>
2019-11-21 16:43:39 -0700

    [READ DESC] Drivetrain Update
    - Completely changed the code to be using the drivetrain feature in vexcode rather than individual motors - much more stable
    - All of the functions work, the robot drives better than ever - both turning and driving velocities now use the axis positions without breaking, and the robot drives perfectly
    - Fully tested, feel free to PR
    - Currently contains: both axes, correctly changing drive velocities, button End to stop the while loop, incrVel, decrVel, stop, maxVel

--- include/robot-config.h
+++ include/robot-config.h
@@ -3,9 +3,8 @@ using namespace vex;
 extern brain Brain;
 
 // VEXcode devices
-extern motor LeftMotor;
-extern motor RightMotor;
 extern controller Controller1;
+extern drivetrain Drivetrain;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Text.
