94fa702043caa75be6e383630d47f7c77279723c
CalculusMaster <31602102+CalculusMaster@users.noreply.github.com>
2019-11-27 23:49:36 -0700

    [READ DESC] Drivetrain Update (#9)
    - Completely changed the code to be using the drivetrain feature in vexcode rather than individual motors - much more stable
    - All of the functions work, the robot drives better than ever - both turning and driving velocities now use the axis positions without breaking, and the robot drives perfectly
    - Fully tested, feel free to PR
    - Currently contains: both axes, correctly changing drive velocities, button End to stop the while loop, incrVel, decrVel, stop, maxVel

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       Saptarshi Mallick, Ojas Sanghi, Khoa Ho                   */
-/*    Created:      Mon Oct 28 2019                                           */
+/*    Author:       C:\Users\1301261566                                       */
+/*    Created:      Thu Nov 21 2019                                           */
 /*    Description:  V5 project                                                */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
@@ -10,175 +10,188 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// LeftMotor            motor         1               
-// RightMotor           motor         2               
 // Controller1          controller                    
+// Drivetrain           drivetrain    1, 10           
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
-#include <iostream>
-#include <sstream>
 
 using namespace vex;
-using namespace std;
 
-int defaultVelocity = 0;
+//Main vars
+int defaultVel() {return 75;}
+int robotVel(){return Drivetrain.velocity(percent);}
+
+  //Axes
+controller::axis axisDrive() {return Controller1.Axis2;}
+controller::axis axisTurn() {return Controller1.Axis4;}
+  //Buttons for increasing and decreasing the robot velocity 
+controller::button bIncrVel() {return Controller1.ButtonUp;}
+controller::button bDecrVel() {return Controller1.ButtonDown;}
+  //Buttons for either setting the robot to velocity 0 (stop), velocity 100 (max), or velocity = default (default)
+controller::button bMaxVel() {return Controller1.ButtonX;}
+controller::button bZeroVel() {return Controller1.ButtonB;}
+controller::button bDefaultVel() {return Controller1.ButtonA;}
+  //Stop program running
+controller::button bEnd() {return Controller1.ButtonY;}
 
 /*
   Stops all motors
+  @param instant Optional parameter to change the method of braking (hold means the wheels stop instantly, brake is the wheels slow down gradually (normal behavior))
 */
-void stopAll()
+void stopAll(bool instant = false)
 {
-  RightMotor.stop();
-  LeftMotor.stop();
+   Drivetrain.setStopping(instant ? hold : brake);
+  Drivetrain.stop();
 }
 
 /*
-  Sets motor velocity to a certain percent
+  CURRENTLY UNUSED
+  Limits the velocity from 10 to 90
 */
-void setTotalVelocity(int perc)
+int limitedVel(int vel)
 {
-  if (perc <= 0) {
-    stopAll();  
-  }
-  RightMotor.setVelocity(perc, percent);
-  LeftMotor.setVelocity(perc, percent);
+  return vel <= 10 ? 20 : (vel >= 90 ? 90 : vel);
 }
 
 /*
-  Drives robot forward or backward
+  Sets the drive velocity to the parameter
+*/
+void setDriveVel(int vel = 0)
+{
+  int absVel = vel < 0 ? vel * -1 : vel;
+  
+  if(absVel == 0) return;
+  
+  Drivetrain.setDriveVelocity(absVel, percent);
+}
+
+/*
+  Changes the drive velocity by the parameter, avoids negatives and values above 100
+*/
+void changeDriveVel(int amount)
+{
+  int fAmt = robotVel() + amount;
+
+  if(fAmt <= 0) fAmt = 0;
+  else if(fAmt >= 100) fAmt = 100;
+
+  setDriveVel(fAmt);
+}
+
+/*
+  Sets the turn velocity to the parameter
+*/
+void setTurnVel(int vel = 100)
+{
+  int absVel = vel < 0 ? vel * -1 : vel;
+
+  if(absVel == 0) return;
+
+  Drivetrain.setTurnVelocity(absVel, percent);
+}
+
+/*
+  Drives forward or backward
 */
 void drive(int vel = 0)
 {
-  directionType dir = vex::forward;
+  directionType driveDir;
 
-  if(vel > 0) dir = vex::forward;
-  else if(vel < 0) dir = vex::reverse;
-  else stopAll();
+  if(vel == 0) return;
+  else if(vel > 0) driveDir = vex::forward;
+  else driveDir = vex::reverse;
 
-  RightMotor.spin(dir);
-  LeftMotor.spin(dir);
+  Drivetrain.drive(driveDir);
 }
 
 /*
-  Default velocity getters and setters
+  Turns left or right
 */
-int getDefaultVel() {
-  return defaultVelocity;
-}
-void setDefaultVel(int vel) {
-  defaultVelocity = vel;
+void turn(int vel = 0)
+{
+  turnType turnDir;
+
+  if(vel == 0) return;
+  else if (vel > 0) turnDir = vex::right;
+  else turnDir = vex::left;
+
+  Drivetrain.turn(turnDir);
 }
 
 /*
-  Spins robot to the left
+  Method to be called during manual control period
 */
-void spinLeft(bool instant = false) 
+void manualControl()
 {
-  if (instant)
-  {
-    setTotalVelocity(100);
-  }
-  LeftMotor.spin(reverse);
-  RightMotor.spin(vex::forward);
-  if (instant) {
-    setTotalVelocity(getDefaultVel());
-  } 
-}
+  int driveAxisPos, turnAxisPos;
+  //bool incr = false, decr = false, max = false, zero = false;
 
-/*
-  Spins robot to the right
-*/
-void spinRight(bool instant = false) 
-{
-  if(instant)
-  {
-    setTotalVelocity(100);
-  }
-  LeftMotor.spin(vex::forward);
-  RightMotor.spin(reverse);
-  if(instant) {
-    setTotalVelocity(getDefaultVel());
-  }
-}
-
-/*
-  Gets the current velocity, or the average of the motors' velocities
-*/
-int getCurrentVelocity()
-{
-  bool isOn = LeftMotor.isSpinning() && RightMotor.isSpinning();
-  if (isOn) {
-    double totalVel = LeftMotor.velocity(percent) + RightMotor.velocity(percent);
-    double avgVel = totalVel / 2;
-    return avgVel;
-  } else {
-    return 0;
-  }
-}
-
-void printVel()
-{
-  ostringstream stream1;
-
-  stream1 << "Right Motor Vel: " << RightMotor.velocity(percent) << " . Left Motor Vel: " << LeftMotor.velocity(percent);
-
-  Brain.Screen.print(RightMotor.velocity(percent));
-  Brain.Screen.print(LeftMotor.velocity(percent));
-}
-
-
-int main()
-{
-  vexcodeInit();
-
-  //Configure Axes here
-  controller::axis axisForwardBackward = Controller1.Axis2;
-  controller::axis axisLeftRight = Controller1.Axis4;
-
-  controller::button incrVel = Controller1.ButtonUp;
-  controller::button decrVel = Controller1.ButtonDown;
-  controller::button resetVel = Controller1.ButtonA;
-
-  controller::button maxVel = Controller1.ButtonX;
-  controller::button zeroVel = Controller1.ButtonB; //Used to be called Master Stop, same functionality
-
-  controller::button instaSpinRight = Controller1.ButtonRight;
-  controller::button instaSpinLeft = Controller1.ButtonLeft;
-
-  setDefaultVel(75);
-  int forwardBackwardPos, leftRightPos;
+  setDriveVel(defaultVel());
+  setTurnVel();
 
   while(true)
   {
-    //Velocity Modifiers
-    if(incrVel.pressing()) setTotalVelocity(getCurrentVelocity() + 1);
-    if(decrVel.pressing()) setTotalVelocity(getCurrentVelocity() - 1);
-    if(resetVel.pressing()) setTotalVelocity(50);
+    //Stop!
+    //if(bEnd().pressing()) break;
 
-    if(maxVel.pressing()) setTotalVelocity(100);
-    if(zeroVel.pressing()) stopAll();
+    //Update axis positions
+    driveAxisPos = axisDrive().position();
+    turnAxisPos = axisTurn().position();
 
-    //Instant Spin
-    if (instaSpinRight.pressing()) {
-      spinRight(true);
-    } else if (instaSpinLeft.pressing()) {
-      spinLeft(true);
-    }
-    
-    //Axis Stuff
-    //Update the axes position values
-    forwardBackwardPos = axisForwardBackward.position();
-    leftRightPos = axisLeftRight.position();
+    /*Update if velocity buttons have been clicked
+    incr = bIncrVel().pressing();
+    decr = bDecrVel().pressing();
+    max = bMaxVel().pressing();
+    zero = bZeroVel().pressing();
 
-    //Stop motors if neither axis in use
-    if(forwardBackwardPos == 0 && leftRightPos == 0) stopAll();
+    //Velocity Modifying Buttons - if any are clicked the while loop will skip driving and stuff
+    if(incr) changeDriveVel(5);
+    if(decr) changeDriveVel(-5);
+    if(max) setDriveVel(100);
+    if(zero) stopAll();
 
-    //Forwards and Backwards (Axis 2)
-    if(forwardBackwardPos != 0) drive(forwardBackwardPos);
+    if(incr || decr || max || zero)
+    {
+      incr = false; decr = false; max = false; zero = false;
+      Controller1.rumble(rumbleShort);
+      continue;
+    }*/
 
-    //Left and Right (Axis 4)
-    if(leftRightPos > 0) spinRight();
-    else if(leftRightPos < 0) spinLeft();
+    //If neither axis is in use, don't do anything
+    if(driveAxisPos == 0 && turnAxisPos == 0) stopAll();
+
+    //Drive
+    if(driveAxisPos != 0) drive(driveAxisPos);
+
+    //Turn
+    if(turnAxisPos != 0) turn(turnAxisPos);
+
+    //Tick Time
+    waitUntil(!Drivetrain.isMoving());
+
+    setDriveVel(driveAxisPos);
+    setTurnVel(turnAxisPos);
   }
+}
+
+/*
+  Method to be called during autonomous period
+*/
+void autonomousControl()
+{
+
+}
+
+int main() 
+{
+  vexcodeInit();
+
+  /* Whenever ready
+  Competition.drivercontrol(manualControl); 
+  Competition.autonomous(autonomousControl);
+  */
+
+  manualControl();
+  return 0;
 }
