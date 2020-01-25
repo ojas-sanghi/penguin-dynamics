55975ecc700b87501aa4f476417ea441016fc1df
CalculusMaster <31602102+CalculusMaster@users.noreply.github.com>
2020-01-24 17:46:32 -0700

    Intake Controls added, code cleaned
    Removed all unnecessary buttons and controls (velocity modifiers)

--- include/robot-config.h
+++ include/robot-config.h
@@ -5,6 +5,8 @@ extern brain Brain;
 // VEXcode devices
 extern controller Controller1;
 extern drivetrain Drivetrain;
+extern motor LeftIntake;
+extern motor RightIntake;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Text.
